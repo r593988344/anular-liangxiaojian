bcaf9f94e3d45261d29869d5a14ec81cfbb9ab21
e <1377056512@qq.com>
2019-05-07 17:37:20 +0800

    添加node后台

--- src/app/content/sensor/sensor-list/sensor-list.service.ts
+++ src/app/content/sensor/sensor-list/sensor-list.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import {SensorList} from './sensor-list';
 import {SensorListMock} from './sensor-list-mock';
-
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class SensorListService {
-  getSensorList(): SensorList[] {
-    return SensorListMock;
+  sensorListUrl =  'http://127.0.0.1:3000/sensorList';
+  getSensorList() {
+    // return SensorListMock;
+    return this.http.get(this.sensorListUrl);
 }
-  constructor() { }
+constructor(private http: HttpClient) { }
 }
