72d5b5333c84acf494824a5895bf586731297f0e
dylan <flowunlight@gmail.com>
2019-05-13 16:49:06 +0800

    change data type

--- src/app/content/sensor/sensor-list/sensor-list-mock.ts
+++ src/app/content/sensor/sensor-list/sensor-list-mock.ts
@@ -1,55 +1,55 @@
 import {SensorList} from './sensor-list';
 export const SensorListMock: SensorList[] = [
-  {
-    collectorNumber: 112233,
-    sensorNumber: 223344,
-    vibrationThreshold: 99,
-    cycle: '30-300',
-    temperature: '25℃',
-    humidity: '25℃',
-    longitude: 135,
-    latitude:  15,
-    sectionId: '2345',
-    creatTime:  '2019-04-30 12:35:04',
-    status: '开启'
-  },
-  {
-    collectorNumber: 123234,
-    sensorNumber: 234234,
-    vibrationThreshold: 23,
-    cycle: '30-233',
-    temperature: '25℃',
-    humidity: '25℃',
-    longitude: 124,
-    latitude:  5,
-    sectionId: '2345',
-    creatTime:  '2019-04-30 12:35:04',
-    status: '开启'
-  },
-  {
-    collectorNumber: 34234,
-    sensorNumber: 3432,
-    vibrationThreshold: 44,
-    cycle: '30-300',
-    temperature: '25℃',
-    humidity: '25℃',
-    longitude: 99,
-    latitude:  23,
-    sectionId: '2345',
-    creatTime:  '2019-04-30 12:35:04',
-    status: '开启'
-  },
-  {
-    collectorNumber: 453433,
-    sensorNumber: 21232,
-    vibrationThreshold: 34,
-    cycle: '30-300',
-    temperature: '25℃',
-    humidity: '25℃',
-    longitude: 133,
-    latitude:  12,
-    sectionId: '2345',
-    creatTime:  '2019-04-30 12:35:04',
-    status: '开启'
-  }
+  // {
+  //   collectorNumber: 112233,
+  //   sensorNumber: 223344,
+  //   vibrationThreshold: 99,
+  //   cycle: '30-300',
+  //   temperature: '25℃',
+  //   humidity: '25℃',
+  //   longitude: 135,
+  //   latitude:  15,
+  //   sectionId: '2345',
+  //   creatTime:  '2019-04-30 12:35:04',
+  //   status: '开启'
+  // },
+  // {
+  //   collectorNumber: 123234,
+  //   sensorNumber: 234234,
+  //   vibrationThreshold: 23,
+  //   cycle: '30-233',
+  //   temperature: '25℃',
+  //   humidity: '25℃',
+  //   longitude: 124,
+  //   latitude:  5,
+  //   sectionId: '2345',
+  //   creatTime:  '2019-04-30 12:35:04',
+  //   status: '开启'
+  // },
+  // {
+  //   collectorNumber: 34234,
+  //   sensorNumber: 3432,
+  //   vibrationThreshold: 44,
+  //   cycle: '30-300',
+  //   temperature: '25℃',
+  //   humidity: '25℃',
+  //   longitude: 99,
+  //   latitude:  23,
+  //   sectionId: '2345',
+  //   creatTime:  '2019-04-30 12:35:04',
+  //   status: '开启'
+  // },
+  // {
+  //   collectorNumber: 453433,
+  //   sensorNumber: 21232,
+  //   vibrationThreshold: 34,
+  //   cycle: '30-300',
+  //   temperature: '25℃',
+  //   humidity: '25℃',
+  //   longitude: 133,
+  //   latitude:  12,
+  //   sectionId: '2345',
+  //   creatTime:  '2019-04-30 12:35:04',
+  //   status: '开启'
+  // }
 ]
